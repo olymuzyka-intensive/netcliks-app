c56b3dfe80ce3123cbfa83cd6bf2bfa60a0d862b
olymuzyka-intensive <137098489+olymuzyka-intensive@users.noreply.github.com>
2024-09-28 16:51:35 +0300

    add angular

---
+++ src/app/constants/films.constants.ts
@@ -0,0 +1,32 @@
+import { Film } from "../models/films.model";
+
+export const FILMS: Film[] = [
+  {
+    id: 0,
+    title: 'Марсианин 1,',
+    originalTitle: 'The Martian 2015',
+    category: 'Фантастика, приключения',
+    rating: 7.8,
+    link: '/film.html',
+    image: './images/film1.jpeg',
+  },
+  {
+    id: 1,
+    title: 'Марсианин 2,',
+    originalTitle: 'The Martian 2015',
+    category: 'Фантастика, приключения',
+    rating: 7.8,
+    link: '/film.html',
+
+    image: './images/film1.jpeg',
+  },
+  {
+    id: 2,
+    title: 'Марсианин 3,',
+    originalTitle: 'The Martian 2015',
+    category: 'Фантастика, приключения',
+    rating: 7.8,
+    link: '/film.html',
+    image: './images/film1.jpeg',
+  },
+];
---
+++ src/app/models/films.model.ts
@@ -0,0 +1,9 @@
+export interface Film {
+  id: number;
+  title: string;
+  originalTitle: string;
+  category: string;
+  rating: number;
+  link: string;
+  image: string;
+}
